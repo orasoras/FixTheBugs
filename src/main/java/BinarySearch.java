af5da0422648acaef77b5b2c9afeb9cf1c0745fe
orasoras <78332013+orasoras@users.noreply.github.com>
2021-02-17 10:40:05 -0500

    Update BinarySearch.java

--- src/main/java/BinarySearch.java
+++ src/main/java/BinarySearch.java
@@ -13,27 +13,25 @@ public class BinarySearch {
 		//create a copy of the original array, so the original wont be sorted 
 		ArrayList<Integer> copyAl = new ArrayList<Integer>(al);
 		QuickSort quickSort = new QuickSort(copyAl);
-    	quickSort.startQuickSort(0, copyAl.size() - 1); 
+    		quickSort.startQuickSort(0, copyAl.size() - 1); 
 		
     	
-    	int low = 0;
+    		int low = 0;
 		int high = copyAl.size() - 1;
 		while(low <= high) {
 			int mid = low + (high - low) / 2;
 			if(copyAl.get(mid) == value) {
-				return true;
-			}
+			    return true;
+			  }
 			else if(copyAl.get(mid) < value) {
-				low = mid +1;
-			}
+			    low = mid +1;
+			   }
 			else {
-				high = mid - 1;
-			}
+			   high = mid - 1;
+			  }
 		}
 		return false;
 	}
 
-	
-	
 }
 
